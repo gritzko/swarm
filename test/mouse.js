242210c879c319ad38422d1a70774352c1746871
Victor Grishchenko <victor.grishchenko@gmail.com>
2013-04-22 18:01:20 +0600

    adj

--- test/mouse.js
+++ test/mouse.js
@@ -13,7 +13,9 @@ var id = Mouse.prototype._type + '#mickey';
 
 var mouse = peer.on(id);
 
-var plumber = new Plumber(peer,'ws://localhost:8000/client');
+var wsServerUri = 'ws://localhost:8000/client';
+
+var plumber = new Plumber(peer,wsServerUri);
 
 peer._on('/=Peer=',function(spec,val){
     window.document.body.setAttribute('connected',plumber.host.getPeerCount()>0?true:false);
@@ -34,16 +36,23 @@ window.onload = function () {
     var x = document.getElementById('x');
     var rtt = document.getElementById('rtt');
     var sample = document.getElementById('sample');
+    var uriSpan = document.getElementById('uri');
 
     x.style.color = userColors[SRC%userColors.length];
     x.innerHTML = userSymbols.charAt(SRC%userSymbols.length);
+    uriSpan.innerHTML = wsServerUri;
     //sample.style.color = userColors[SRC%userColors.length];
     //sample.innerHTML = userSymbols.charAt(SRC%userSymbols.length);
 
     document.body.onmousemove = function (event) {
-        mouse.setX(event.clientX);
+        /*mouse.setX(event.clientX);
         mouse.setY(event.clientY);
-        mouse.setMs(new Date().getTime()); // TODO bundle
+        mouse.setMs(new Date().getTime()); // TODO bundle*/
+        mouse.set({
+            x: event.clientX,
+            y: event.clientY,
+            ms: new Date().getTime()
+        });
     };
 
     mouse.on('',function(spec,val){
