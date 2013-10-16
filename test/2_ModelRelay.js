519651c2ee4257b57cd0cb3d62def521cf062283
Victor Grishchenko <victor.grishchenko@gmail.com>
2013-10-16 17:49:23 +0400

    final EventRelay&Co signatures

--- test/2_ModelRelay.js
+++ test/2_ModelRelay.js
@@ -3,80 +3,154 @@
  * User: gritzko
  * Date: 8/24/13
  * Time: 6:21 PM
- * To change this template use File | Settings | File Templates.
  */
 
 if (typeof require == 'function') {
-    var Swarm = require('../lib/swarm2.js');
-    Spec = Swarm.Spec;
-    Model = Swarm.Model;
+    var swrm = require('../lib/swarm2.js');
+    Spec = swrm.Spec;
+    Swarm = swrm.Swarm;
+    Model = swrm.Model;
+    Field = swrm.Field;
 } else {
     exports = this.testModelRelay = {};
 }
 
-function Duck (name) {
-    this.init(name);
-    this.age = 0;
-}
+// BACK TO SANITY
+//   0 write the test for tracked props/logged methods/serial/etc : the API end
+// V 1 nice stacks, extend only, constructors, no surrogates
+// X 2 introspection back NO use addXXX() instead
+//   3 trace it all along; get an intuition
+// V 4 init(): parent.children[id] = this;   (no create();
+//     new>init>children call chain instead)
+//
+// USABILITY TODOs
+//   1 new Model(id) OR new Model(values)
 
-Duck.prototype.grow = function grow () {
-    this.setAge(this.age+1);
+function NumberField (id) {
+    this.init(id);
+}
+Field.extend(NumberField,{
+    validate: function (args) {
+        return typeof(args.value)==='number';
+    }
+});
+
+function MetricLengthField (id) {
+    this.init(id);
+}
+Field.extend(MetricLengthField,{
+    apply: function (args) {
+        // convert mm cm m km
+    },
+    validate: function (args) {
+        return typeof(args.value)==='number';
+    },
+    toString: function () {
+    }
+});
+
+// Duck is our core testing class :)
+function Duck (id) {
+    this.init(id);
+    this.mood = 'neutral';
 };
 
-Swarm.author = '&gritzko';
-Swarm.Model.extend(Duck, {});
+Duck.prototype.canDrink = function () {
+    return this.age() >= 18;
+};
+
+Duck.prototype.mood = function (moodStr) {
+    this.mood = moodStr;
+};
+
+Model.extend(Duck);
+Swarm.addType(Duck);  // Model by default 
+
+Duck.addProperty('age',0,NumberField);
+Duck.addProperty('height','5cm',MetricLengthField);
+Duck.addMethod(function grow(cm){});
+Duck.addCall(function reportAge(){});
+Duck.addCall('reportAge');
 
 
 exports.setUp = function (cb) {
+    swarm = new Swarm('gritzko');
+    //Swarm.author = 'gritzko';
     cb();
 };
 
 exports.testListener = function (test) {
     var huey = new Duck('Huey');
-    huey.on('age',function(key,val){
+    huey.on('age',function(spec,val){
         test.equal(val,1);
-        test.equal(key.member,'age');
+        test.equal(spec.member,'age');
         test.done();
     });
-    huey.setAge(1);
+    huey.age(1);
+};
+
+exports.testCreate = function (test) {
+    var dewey1 = new Duck('dewey');
+    var dewey2 = DuckModel.child('dewey');
+    test.strictEqual(dewey1,dewey2);
+    test.equal(dewey1.type,DuckModel);
 };
 
 exports.testRelay = function (test) {
-    var dewey1 = new Duck('dewey');
-    var dewey2 = new Duck('dewey');
-    dewey1.on(dewey2);
-    dewey2.on('age',{
-        set : function (spec,val) {
-            test.equal(spec.toString(),'/Duck#dewey.age');
-            test.equal(val,2);
+    var dewey = new Duck('dewey');
+    dewey.on('age',{
+        set : function (args) {
+            test.equal(args.spec.toString(),'/Duck#dewey.age');
+            test.equal(args.value,2);
             var so = new Spec(spec);
-            test.equal(so.author,'gritzko');
+            test.equal(Spec.ext(args.spec.vid),'gritzko');
             test.done();
         }
     });
-    dewey1.set('age',2);
+    dewey.set('age',2);
 };
 
 exports.testVids = function (test) {
     var louie = new Duck('louie');
-    var ts1 = Spec.ts();
-    louie.setAge(3);
-    var ts2 = Spec.ts();
+    var ts1 = Spec.vid();
+    louie.age(3);
+    var ts2 = Spec.vid();
     test.ok(ts2>ts1);
-    var vid = louie._vid.age;
+    var vid = louie.properties.age.version;
     test.ok(ts1<vid);
     test.ok(ts2>vid);
     test.done();
 };
 
+exports.testJSON = function (test) {
+    var duckJSON = {
+        mood: 'merry',
+        properties: {
+            age: 0
+        }
+    };
+
+};
+
 exports.testStaticCallbacks = function (test) {
     var huey = new Duck('huey');
     test.expect(2);
-    Duck.prototype.onAge = function (value) {
+    Duck.prototype.onto ('age', function (spec,value) {
         test.equal(value,1);
-    };
+    });
     var vid = Spec.vid();
-    huey.set('/Duck#huey.age'+vid,1);
-    test.equal(huey._vid.age,vid);
+    huey.set('/Duck#huey.age!'+vid,1);
+    test.equal(huey.properties.age.version,vid);
+    test.done();
+};
+
+exports.testOnce = function (test) {
+    var huey = DuckModel.child('huey');
+    test.expect(1);
+    huey.once('age',function(spec,value){
+        test.equal(value,4);
+    });
+    huey.age(4);
+    huey.age(5);
     test.done();
 };
