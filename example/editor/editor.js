a7383a9f3a0f6a3da1fc9a3b32b77a12eddb59b5
Victor Grishchenko <victor.grishchenko@gmail.com>
2014-06-21 15:50:23 +0400

    fixed: state init races

--- example/editor/editor.js
+++ example/editor/editor.js
@@ -1,6 +1,9 @@
 var storage = new Swarm.SharedWebStorage(false);
-var host = Swarm.localhost = new Swarm.Host('gritzko',0,storage);
+var hash = window.location.hash || '#0';
+var host = Swarm.localhost = new Swarm.Host('me'+hash.replace('#','~'),0,storage);
 host.availableUplinks = function () {return [storage]};
+if (hash=='#1')
+    storage.authoritative = true;
 Swarm.localhost = host;
 Swarm.debug = true;
 var textarea = document.getElementById('text');
@@ -8,20 +11,21 @@ var textarea = document.getElementById('text');
 var text = new Swarm.Text('/Text#note');
 
 text.on(function(){
-    textarea.value = text.text;    
+    textarea.value = text.text;
+    console.log('textarea refresh',text.text);
 });
 
-if (!text.text) {
-    text.set("Hello world!");    
+if ('$pending_dl$' in text._lstn[2])   // TODO explain no authoritative storage => stalls
+    console.error('should not happen');
+
+if (!text.text && host._id==='me~1') {
+    text.set("Hello world!");   
 }
 
-
-console.log('haha',text.text);
 
 textarea.onkeyup = function textChange(ev) {
     var el = ev.target;
     var value = el.value;
     if (value!=text.text)
         text.set(value);
-    console.log(value);
 }
