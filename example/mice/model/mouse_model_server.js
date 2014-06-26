ef29ea6dc99969e14331fbf329342f841b1357c4
Alexey Balandin <aleksisha@gmail.com>
2014-06-26 16:52:13 +0400

    Host.off/reoff

--- example/mice/model/mouse_model_server.js
+++ example/mice/model/mouse_model_server.js
@@ -4,10 +4,11 @@ if (typeof exports === 'object') {
 
 var moribund = {};
 
-function miceClean () {
-    var time = new Date().getTime();
-    var host = Swarm.localhost;
-    var online = {};
+function miceClean() {
+    var time = new Date().getTime(),
+        host = Swarm.localhost,
+        online = {},
+        id;
     for (var src in host.sources) {
         var m = src.match(/([A-Za-z0-9_\~]+)(\~[A-Za-z0-9_\~]+)/);
         if (!m) { console.error('alien',src); continue; }
@@ -16,26 +17,30 @@ function miceClean () {
     }
     var mice = host.get('/Mice#mice');
     if (!mice._version) return;
+
     var objects = mice.objects;
-    for(var s in objects) {
+    for (var s in objects) {
         var spec = new Swarm.Spec(s);
         if (spec.type()!=='Mouse') continue;
-        var id = spec.id();
+
+        id = spec.id();
         if (id in online) {
             delete moribund[id];
             continue;
-        } else {
-            console.error(id,'not in sources');
         }
         if (id in moribund) continue;
+
+        console.error(id,'not in sources');
         moribund[id] = time;
     }
     var ancient = time - 10*1000;
-    for(var id in moribund) {
+    for (id in moribund) {
         var ts = moribund[id];
-        if (ts<ancient) mice.removeObject('/Mouse#'+id);
-        delete moribund[id];
+        if (ts < ancient) {
+            mice.removeObject('/Mouse#'+id);
+            delete moribund[id];
+        }
     }
 }
 
-setInterval(miceClean,1000);
+setInterval(miceClean, 1000);
