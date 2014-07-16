3b7ffb68cffd206685c4f7d02c38a98ea5ef5763
Alexey Balandin <aleksisha@gmail.com>
2014-07-16 23:24:51 +0400

    fix Error "Invariant Violation: forceUpdate(...): Can only force an update on mounted or mounting components."

--- example/mice/mice_view.js
+++ example/mice/mice_view.js
@@ -5,6 +5,7 @@ var app = app || {};
 
     var SwarmMixin = {
         deliver: function (spec,val,source) {
+            if (!this.isMounted()) return;
             this.forceUpdate();
         },
         componentWillMount: function () {
