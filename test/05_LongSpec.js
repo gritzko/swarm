d81e3cbf4932a7ddc72b40e17ca7404e1ad6c691
Victor Grishchenko <victor.grishchenko@gmail.com>
2014-09-13 20:33:11 +0400

    Iterator-based chunked LongSpec implementation
    It is now fit to make super-long weaves (like "Word and Peace" long).
    I would die coding this if not for Korpiklaani music.
    Thanks to the guys, I am descending to the Underworld instead.

--- test/05_LongSpec.js
+++ test/05_LongSpec.js
@@ -8,7 +8,6 @@ var Host = require('../lib/Host');
 
 test('5.a unicode (base 2^15) numbers', function (test) {
     // encode
-    var base64 = 'ABCDEF';
     var numf = Spec.base2int('ABCDE'); // 6*6 = 36 bit, 3 uni
     var num = (0xE) + (0xD<<6) + (0xC<<12) + (0xB<<18) + (0xA<<24);
     var suffix = 0xf;
@@ -16,12 +15,15 @@ test('5.a unicode (base 2^15) numbers', function (test) {
     var uni = String.fromCharCode(0x30+(numf>>15), 0x30+(numf&0x7fff));
     var unif = LongSpec.int2uni(numf);
     equal(unif,uni);
-    var suff = String.fromCharCode(suffix+0x30);
-    equal(LongSpec.base2uni3(base64), uni+suff);
     // decode
-    var de = LongSpec.uni3base(uni+suff);
-    equal(de,base64);
-    equal(LongSpec.uni2base(LongSpec.base2uni2('0')), '0');
+    var de = LongSpec.uni2int(unif);
+    equal(de,numf);
+
+    for(var i=0; i<=0x7fff; i++) {
+        var u = LongSpec.int2uni(i);
+        var i2 = LongSpec.uni2int(u);
+        if (i!==i2) { equal(i,i2); }
+    }
 });
 
 test('5.b constructor', function (test){
@@ -31,7 +33,28 @@ test('5.b constructor', function (test){
     equal(str, '/ab#cd!ef+gh.ij');
 });
 
-test('5.c encode/decode', function (test){
+test('5.c1 encode - sequences', function (test){
+
+    var ls = new LongSpec('#unencodeable#unencodeable#unencodeable');
+    equal(ls.chunks[0],'#unencodeable##')
+
+    var book = {en:{'.on':'.0','+ext':'+e'}};
+    var repeats = new LongSpec('.on.on.on.on.on.on.on.on.on.on', book);
+    equal(repeats.chunks[0], '.0.........');
+
+    var numbers = new LongSpec('!shrt1!shrt2!shrt3');
+    var uni = LongSpec.int2uni(Spec.base2int('shrt1'));
+    equal(numbers.chunks[0], '!'+uni+'!!');
+
+    var exts = new LongSpec('!shrt1+ext!shrt2+ext!shrt3+ext', book);
+    equal(exts.chunks[0], '!'+uni+'+e!!');
+
+    var longs = new LongSpec('#longnum001#longnum002');
+    equal(longs.chunks[0].length, 6);
+
+});
+
+test('5.c3 general encode/decode', function (test){
     var codeBook = {
         en : {
             '/Mouse': '/M',
@@ -40,25 +63,27 @@ test('5.c encode/decode', function (test){
             '#Mickey':'#i'
         }
     };
-    var ls = new LongSpec('',codeBook);
-
     var spec1 = '/Mouse#Mickey!abcde.on';
-    var enc1 = ls.encode(spec1);
-    var dec1 = ls.decode(enc1);
-    equal(dec1.toString(), spec1);
-    equal(enc1.toString(),'/M#i!\u4b64\u7a59.o');
+    var ls1 = new LongSpec(spec1,codeBook);
+    equal(ls1.chunks[0],'/M#i!\u4b64\u7a59.o');
+    equal(spec1,ls1.toString());
 
     var spec2 = '/Mouse#Mickey!bcdef.off';
-    var enc2 = ls.encode(spec2);
-    var dec2 = ls.decode(enc2);
-    equal(spec2,dec2);
-    equal(enc2,'/M#i!\u4d6d\u0a9a.f');
+    var ls2 = new LongSpec(spec2,codeBook);
+    equal(ls2.chunks[0],'/M#i!\u4d6d\u0a9a.f');
+    equal(spec2,ls2.toString());
 
     var spec3 = '/Mouse#abcde.off';
-    var enc3 = ls.encode(spec3);
-    var dec3 = ls.decode(enc3);
-    equal(spec3,dec3);
-    equal(enc3,'/M#\u4b64\u7a59.f');
+    var ls3 = new LongSpec(spec3,codeBook);
+    equal(ls3.chunks[0],'/M#\u4b64\u7a59.f');
+    equal(spec3,ls3.toString());
+
+    var zeros5 = new LongSpec('.00000');
+    equal(zeros5.toString(),'.00000');
+
+    var zeros7 = new LongSpec('.0000001');
+    equal(zeros7.toString(),'.0000001');
+
 });
 
 test('5.d find & partials', function (test){
@@ -83,26 +108,14 @@ test('5.e edits and O(n)', function (test){
     }
     var at;
     while ( (at = longls.find('.bc',at?at.index+1:at)) && count--) {
-        at.insertDe('.a');
+        at.insert('.a');
     }
     var spec = longls.toString();
     equal(spec, '.a.bc.a.bc.a.bc.a.bc');
 });
 
-/*test('5.f matching and iteration', function (test){
-    var lsstr = '/Text#note!abcde.in!bcdefgh+author.rm';
-    var ls = new LongSpec(lsstr);
-    var pattern = '!=(\\+=)?.=';
-    var compiled = LongSpec.compilePattern(pattern);
-    var m = ls.findPattern(pattern);
-    equal(ls.match(m),'!abcde.in');
-    var m2 = ls.findPattern(compiled,m);
-    equal(ls.match(m2),'!bcdefgh+author.rm');
-    var m3 = ls.findPattern(compiled,m2);
-    equal(m3,null);
-});
 
-test('5.g mass encode/decode', function (test) {
+/*test('5.g mass encode/decode', function (test) {
     var epoch = 1262275200000; // TODO move to Spec
     var time = ((new Date().getTime()-epoch)/1000)|0;
     for(var i=0; i<100; i++) {
@@ -132,23 +145,21 @@ test('5.h Array-like API', function(test) {
 test('5.i Iterator', function(test) {
     var ls = new LongSpec('!one#two.three/four4');
     var i = ls.iterator();
-    equal(i.de(),'!one');
+    equal(i.token(),'!one');
     i.next();
-    equal(i.de(),'#two');
+    equal(i.token(),'#two');
 
     var e = ls.iterator();
     e.skip(3);
-    equal(e.de(),'/four4');
+    equal(e.token(),'/four4');
     e.next();
     ok(e.end());
     equal(e.token(),undefined);
-    equal(e.de(),undefined);
 
     var e300 = ls.iterator();
     e300.skip(300);
     ok(e300.end());
     equal(e300.token(),undefined);
-    equal(e300.de(),undefined);
 
     var lx = new LongSpec('!one#two.three/four4');
     var x = lx.iterator();
@@ -158,38 +169,54 @@ test('5.i Iterator', function(test) {
 
     var j = ls.iterator(); // still !one#two.three/four4
     j.skip(2);
-    equal(j.de(),'.three');
+    equal(j.token(),'.three');
     j.erase(2);
     equal(j.index,2); // eof
-    j.insertDe('#two+andahalf');
+    j.insert('#two+andahalf');
     equal(j.index,3);
     equal(ls.length(),3);
     equal(ls.toString(),'!one#two#two+andahalf');
 
     var k = ls.iterator();
     k.skip(2);
-    equal(k.de(),'#two+andahalf');
+    equal(k.token(),'#two+andahalf');
 
     var l = ls.iterator(); // !one#two#two+andahalf
     equal(l.index,0);
-    l.insertDe('/zero');
+    l.insert('/zero');
     equal(ls.length(),4);
-    equal(l.de(),'!one');
+    equal(l.token(),'!one');
     equal(l.index,1);
 
     var empty = new LongSpec();
     var ei = empty.iterator();
     ok(ei.end());
-    ei.insertDe('!something+new'); // FIXME throw on format violation
+    ei.insert('!something+new'); // FIXME throw on format violation
     equal(empty.toString(), '!something+new');
 });
 
-/*test('5.j Sequential compression', function(test) {
-    var ls = new LongSpec('!abc!abd!abe!abf');
+test('5.j Sequential compression', function(test) {
+    var ls = new LongSpec('!00abc!00abd!00abe!00abf');
     var i = ls.end();
-    i.insert('!abg!abh!abi');
-    ok(ls.charSize()<10);
-});*/
+    i.insertBlock('!00abg!00abh!00abi');
+    equal(ls.length(),7);
+    ok(ls.charLength()<10);
+    var f = ls.find('!00abe');
+    equal(f.index,2);
+    var v = ls.find('!00abh');
+    equal(v.index,5);
+    var j = ls.iterator(4);
+    equal(j.token(),'!00abg');
+
+    var lse = new LongSpec('!~~~a1+src!~~~a2+src!~~~a3+src');
+    var ei = lse.iterator();
+    ei.next();
+    equal(ei.token(),'!~~~a2+src');
+    equal(ei.match[0].length,1);
+});
+
+// test TODO   max-uni insert
+// test TODO   multiple inserts, iterator wear
 
 /*test('5.7 sequential coding', function (test) {
     var clockContext = {
