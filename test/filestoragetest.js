2e6ba2c09f9fdf6215d4b482902093810c3096f2
Victor Grishchenko <victor.grishchenko@gmail.com>
2014-09-23 16:11:46 +0400

    Storage-based FileStorage implementation

--- test/filestoragetest.js
+++ test/filestoragetest.js
@@ -8,6 +8,8 @@ var nopt = require('nopt'),
     }),
     Swarm = require('../lib/NodeServer');
 
+Swarm.env.debug = true;
+
 var Counter = Swarm.Model.extend('Counter',{
     defaults: {
         i: 0
@@ -16,36 +18,28 @@ var Counter = Swarm.Model.extend('Counter',{
 
 var store = './.store/',
     storage = new Swarm.FileStorage(store),
-    host = new Swarm.Host('swarm', 0, storage),
-    i;
+    host = new Swarm.Host('swarm', 0, storage);
 
 Swarm.localhost = host;
 
 Swarm.debug = true;
 
 var counters = [];
-for (i = 0; i < 100; i++) {
+for (var i = 0; i < 100; i++) {
     counters.push(new Counter('counter' + i));
 }
-
-// FIXME wait for load
-/*if (!firstRun) {
-    for (var i=0; i<1000; i++)
-        console.log(i,counters[i].i);
-}*/
 
 switch (options.mode) {
 case 'write':
 
     // half a million ops must be enough
-    for (i = 0; i < 100; i++) {
+    for (i = 0; i < 10; i++) {
         for (var k = i; k < 100; k++) {
             counters[k].set({i: i}); // :)
         }
     }
 
-    storage.rotateLog();
-    storage.pullState();
+    storage.close();
 
     break;
 
@@ -53,16 +47,8 @@ case 'check':
 
     setTimeout(function(){ // TODO collection onload()
         for (var i = 0; i < 100; i++) {
-            /* jshint
-            if (!counters[i]._version) {
-            } else if (i === counters[i].i) {
-            }
-            */
             if (counters[i]._version) {
                 console.log(i, counters[i].i);
-                //delete counters[i]._host;
-                //delete counters[i]._lstn;
-                //console.log(JSON.stringify(counters[i]),' === ',i);
             }
         }
     },4000);
@@ -73,7 +59,7 @@ default:
     console.error('unrecognized mode: ' + options.mode);
 }
 
-// exit before it settles
+// exit violently
 //setTimeout(function(){
 //    process.exit(1);
 //},1);
