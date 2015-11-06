dcc382093a2fbe18c7c4ed8b7ad1560a5f9d0cdc
Eugene Girshov <eugene@solu.co>
2015-11-06 17:21:53 +0200

    update gw to use 1.0 api

--- gw/src/Gateway.js
+++ gw/src/Gateway.js
@@ -1,7 +1,7 @@
 "use strict";
-var Swarm = require("../../"); // FIXME multipackage
-var Spec = Swarm.Spec;
-var Op = Swarm.Op;
+var Syncable = require('swarm-syncable');
+var Spec = Syncable.Spec;
+var Op = Syncable.Op;
 
 //
 //    client ---> gateway ---> logics ---> host ---> storage, server
@@ -12,21 +12,40 @@ var Op = Swarm.Op;
 
 /** mission: no versioning, screens, still asynchronous, even further
   * then at the transport level */
-function Gateway (logics) {
+function Gateway (host) {
     this.onChange = this.onChangeProto.bind(this);
-    this.logics = logics;
+    this.host = host;
     this.streams = {};
     this.subscribers = {};
 }
 module.exports = Gateway;
 
+Gateway.prototype.removeStream = function (stream_id) {
+    delete this.streams[stream_id];
+    for (var spec in this.subscribers) {
+        this.subscribers[spec] = this.subscribers[spec].filter(function (x) { return x != stream_id; });
+    }
+}
+
 Gateway.prototype.addStream = function (stream_id, stream) {
     var self = this;
     var unparsed = '';
     this.streams[stream_id] = stream;
-    stream.on('data', function(data){
+    stream.on('data', function(data) {
         unparsed += data.toString();
-        var parsed = Op.parse(unparsed, stream_id);
+        if (unparsed == '\n') {
+            stream.write('\n');
+            unparsed = '';
+            return;
+        }
+        var parsed;
+        try {
+            parsed = Op.parse(unparsed, stream_id);
+        } catch (e) {
+            console.log('Op.parse error: ', e);
+            return;
+        }
+
         unparsed = parsed.remainder;
         // seek \n => parse the op
         for (var i=0; i<parsed.ops.length; i++) {
@@ -46,15 +65,15 @@ Gateway.prototype.receive = function (op) {
     }
 };
 
-var abbrev = new Spec.Parsed('/Model.ON');
+var abbrev = new Spec.Parsed('/Model');
 
 function abbreviate (spec, stream_stamp) {
-    abbrev.stamp = stream_stamp;
+    abbrev._stamp = stream_stamp;
     return new Spec.Parsed(spec).toString(abbrev);
 }
 
 function unabbreviate (spec, stream_stamp) {
-    abbrev.stamp = stream_stamp;
+    abbrev._stamp = stream_stamp;
     return new Spec.Parsed(spec, abbrev).toString();
 }
 
@@ -70,17 +89,21 @@ Gateway.prototype.onChangeProto = function (ev) {
 Gateway.prototype.sendState = function (obj, stream_id) {
     var stream = this.streams[stream_id];
     var json = obj.toString();
-    var ver = new Spec(obj._version).get('!') || '0';
+    var ver = obj._version || '0'; // new Spec(obj._version).get('!') || '0';
     var evspec = obj.spec().add(ver, '!').add('.STATE'); // no !ver
     evspec = abbreviate(evspec);
-    stream && stream.write(evspec+'\t'+json+'\n');
+    try {
+        stream && stream.write(evspec + '\t' + json + '\n');
+    } catch (e) {
+        console.log('sendState error:', e);
+    }
 };
 
 // a new subscriber for an object
 Gateway.prototype.ON = function (op, stream_id, once) {
     var spec = op.spec.filter('/#');
     var subs = this.subscribers[spec], self = this;
-    var obj = this.logics.get(op.spec);
+    var obj = this.host.get(spec);
     //if (obj.hasState()) {
     //    this.sendState(obj, stream_id);
     //} // else the 'init' event will trigger our onChange
@@ -88,7 +111,7 @@ Gateway.prototype.ON = function (op, stream_id, once) {
         subs.push(stream_id);
     } else {
         subs = this.subscribers[spec] = [stream_id];
-        obj.on(self.onChange); // TODO test for machinegunning
+        obj.on('change', self.onChange); // TODO test for machinegunning
     }
     obj.onInit(function(){
         self.sendState(obj, stream_id);
@@ -122,23 +145,15 @@ Gateway.prototype.OFF = function (op, stream_id) {
 Gateway.prototype.STATE = function (op, stream_id) {
 
     var spec = op.spec.filter('/#');
-
-    if (!op.id()) { // create if new
-        // create a zero-state object and subscribe to it
-        var outer = this.logics.create(spec.type());
-        // assuming the storage is asynchronous, this will generate
-        // an event asynchronously, with all the state injected below
-    } else {
-        outer = this.logics.get(spec);
+    var json;
+    try {
+        json = JSON.parse(op.value);
+    } catch (e) {
+        console.log('Parse error:', e);
+        return;
     }
-    // rape the outer state
-    var pojo = JSON.parse(op.value);
-    for(var key in pojo) {
-        if (Swarm.Syncable.reFieldName.test(key)) {
-            outer[key] = pojo[key];
-        }
-    }
-    outer.save();
+    var outer = this.host.get(spec);
+    outer.set(json);
     // this triggers events that trigger onChange, so the stream gets a response
     if (!op.id()) {
         this.ON(new Op(outer.spec(), ''), stream_id);
