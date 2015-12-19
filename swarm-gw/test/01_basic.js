3136eb3213a64d2314f7cc387402b66b9868eb5b
Eugene Girshov <eugene@solu.co>
2015-12-19 22:32:25 +0200

    gw: more tests added

--- swarm-gw/test/01_basic.js
+++ swarm-gw/test/01_basic.js
@@ -1,7 +1,7 @@
 "use strict";
 var Swarm = require('..');
 var Gateway = Swarm.Gateway;
-var Syncable = Swarm.Syncable;
+var only = require('tap').test;
 var tape = require('tap').test;
 
 var bat = require('swarm-bat');
@@ -79,39 +79,79 @@ tape ('Gateway.1.C.two streams', function (tap) {
 
 tape ('Gateway.1.D.created through host', function (tap) {
     var stream = new BatStream();
-
+    var received = 0;
     var host = createHost();
     var gateway = new Gateway(host);
     gateway.addStream('stream', stream.pair);
 
     var obj = host.get('/Model');
-    obj.onInit(function () {
-        obj.set({key: 'test value'});
-        stream.write('/Model#' + obj._id + '.ON\t\n');
-        stream.on('data', function (data) {
-            var reply = data.toString();
-            var json = JSON.parse(reply.split('\t')[1]);
-            tap.equal(json.key, 'test value');
-            host.close();
-            tap.end();
-        });
+    // Locally created object is immediately ready, no need to use onInit
+    obj.set({key: 'test value'});
+
+    stream.write('/Model#' + obj._id + '.ON\t\n');
+    stream.on('data', function (data) {
+        var reply = data.toString();
+        var json = JSON.parse(reply.split('\t')[1]);
+        tap.equal(json.key, 'test value');
+        received += 1;
     });
+    setTimeout(function () {
+        tap.equal(received, 1, 'Expect to receive exactly one reply');
+        host.close();
+        tap.end();
+    }, 100);
 });
 
 tape ('Gateway.1.E.updated through gateway', function (tap) {
     var stream = new BatStream();
-
+    var received = 0;
     var host = createHost();
     var gateway = new Gateway(host);
     gateway.addStream('stream', stream.pair);
 
     var obj = host.get('/Model');
-    obj.onInit(function () {
-        stream.write('/Model#' + obj._id + '.STATE\t{"key":"another value"}\n');
-    });
     obj.on('change', function () {
         tap.equal(obj['key'], 'another value');
+        received += 1;
+    });
+    stream.write('/Model#' + obj._id + '.STATE\t{"key":"another value"}\n');
+    setTimeout(function () {
+        tap.equal(received, 1, 'Expect to receive exactly one change event');
         host.close();
         tap.end();
+    }, 100);
+});
+
+tape ('Gateway.1.F.add and remove stream', function (tap) {
+    var received1 = 0, received2 = 0;
+    var host = createHost();
+    var gateway = new Gateway(host);
+
+    var obj = host.get('/Model');
+    obj.set({key: 'initial value'});
+
+    var stream1 = new BatStream();
+    var stream2 = new BatStream();
+    gateway.addStream('stream1', stream1.pair);
+
+    stream1.write('/Model#' + obj._id + '.ON\t\n');
+    stream1.on('data', function (data) {
+        tap.equal(JSON.parse(data.toString().split('\t')[1]).key,
+                  'initial value');
+        received1 += 1;
+        gateway.removeStream('stream1');
+        gateway.addStream('stream2', stream2.pair);
+        stream2.write('/Model#' + obj._id + '.ON\t\n');
     });
+    stream2.on('data', function (data) {
+        tap.equal(JSON.parse(data.toString().split('\t')[1]).key,
+                  'initial value');
+        received2 += 1;
+    });
+    setTimeout(function () {
+        tap.equal(received1, 1, 'Expect to receive exactly one reply in first stream');
+        tap.equal(received2, 1, 'Expect to receive exactly one reply in second stream');
+        host.close();
+        tap.end();
+    }, 100);
 });
