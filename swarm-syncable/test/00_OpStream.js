7d98bab473ad7e0cda4d4a9ed1b37495ee9e5c9c
Victor Grishchenko <victor.grishchenko@gmail.com>
2016-08-11 20:03:39 +0500

    more comments for the example test

--- swarm-syncable/test/00_OpStream.js
+++ swarm-syncable/test/00_OpStream.js
@@ -6,7 +6,9 @@ let Op = swarm.Op;
 
 tape ('syncable.00.A echo op stream - event filtering', function (t) {
 
+    // OpStream is a semi-abstract base class, an echo stream
     let stream = new OpStream();
+    // ops to emit
     let ops = Op.parseFrame(
         "/Swarm#test!time.off\n" +
         "/Object#7AM0f+gritzko!0.on\n" +
@@ -17,9 +19,12 @@ tape ('syncable.00.A echo op stream - event filtering', function (t) {
     let ons = 0, onoffs = 0, states = 0, mutations = 0, myobj = 0;
     let unsub = false;
 
+    // OK, let's play with filters and listeners
     stream.on(".on", op => ons++);
+    // may use stream.onHandshake(op => onoffs++)
     stream.on(".on.off", op => onoffs++);
     stream.on(".~", op => states++);
+    // may use stream.onMutation(op=> mutations++)
     stream.on("^.on.off.error.~", op=> mutations++);
     stream.on("/Swarm.off", op=> unsub=true);
     stream.on("/Object#7AM0f+gritzko!0", on => {
@@ -27,6 +32,7 @@ tape ('syncable.00.A echo op stream - event filtering', function (t) {
         t.ok(on.isOn());
     });
 
+    // may use stream.on ( null, () => {...} )
     stream.onEnd(nll => {
         t.equals(nll, null);
         t.equals(ons, 1);
@@ -37,7 +43,9 @@ tape ('syncable.00.A echo op stream - event filtering', function (t) {
         t.end();
     });
 
+    // feed all the ops into the echo stream to trigger listeners
     stream.offerAll(ops);
-    stream.offer(null);
+    // stream.offer(null) has the same effect as stream.end()
+    stream.end();
 
 });
