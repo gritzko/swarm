7fd5176f9ff935c3a3123514a8d5b6daa55e3008
Victor Grishchenko <victor.grishchenko@gmail.com>
2016-09-27 15:18:28 +0500

    specify explicit package dependency DAG

--- scripts/add-links.sh
+++ scripts/add-links.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 echo cross-symlinks to working tree versions of swarm packages
 ROOT=`pwd`
-PACKAGES=`ls -d swarm-*`
+PACKAGES="swarm-bat swarm-protocol swarm-syncable swarm-cli swarm-peer
+swarm-server swarm-browser swarm-gw"
+DEPS=""
 for PACK in $PACKAGES; do
     cd $PACK
     if [ ! -e node_modules ]; then
         mkdir node_modules;
     fi
     cd node_modules
-    for LINK in $PACKAGES; do
+    for LINK in $DEPS; do
         if [ ! -e $LINK ]; then
             ln -s $ROOT/$LINK $LINK
         fi
     done
+    DEPS="$DEPS $PACK"
     cd ..
     cd ..
 done
