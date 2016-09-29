18530049e5684adc003476527917c54fb9fa5889
Victor Grishchenko <victor.grishchenko@gmail.com>
2016-09-29 13:33:57 +0500

    remove an accidental newline

--- scripts/add-links.sh
+++ scripts/add-links.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 echo cross-symlinks to working tree versions of swarm packages
 ROOT=`pwd`
-PACKAGES="swarm-bat swarm-protocol swarm-syncable swarm-cli swarm-peer
-swarm-server swarm-browser swarm-gw"
+PACKAGES="swarm-bat swarm-protocol swarm-syncable swarm-cli swarm-peer swarm-server swarm-browser swarm-gw"
 DEPS=""
 for PACK in $PACKAGES; do
     cd $PACK
@@ -13,6 +12,8 @@ for PACK in $PACKAGES; do
     for LINK in $DEPS; do
         if [ ! -e $LINK ]; then
             ln -s $ROOT/$LINK $LINK
+        else
+            echo $PACK already has $LINK
         fi
     done
     DEPS="$DEPS $PACK"
