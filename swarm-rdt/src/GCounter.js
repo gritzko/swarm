db1bb49c0b189fe6326ee4293dab605dfe123ec5
Victor Grishchenko <swarm.js@gmail.com>
2017-06-05 15:48:36 +0200

    basic GCounter impl

--- swarm-rdt/src/GCounter.js
+++ swarm-rdt/src/GCounter.js
@@ -12,35 +12,67 @@ class GCounter extends RDT {
         this._value = 0;
     }
 
+    value () {
+        return this._value;
+    }
+
+    add (num) {
+        if (!Number.isInteger(num) || num<=0)
+            throw new Error("not a positive integer");
+        if (num+this._value>Number.MAX_SAFE_INTEGER)
+            throw new Error("integer overflow"); // another failure state?
+        this.submit("add", num);
+    }
+
     _update (new_state_i, changes_i) {
-        const state_op = new_state_i.next().value;
-        if (!state_op) {
+        const state_op = new_state_i.op;
+        if (new_state_i.end()) {
             this._value = 0;
-        } else if (state_op.isError() || !state_op.Location.eq(GCounter.SUM_UUID)) {
+        } else if (state_op.isError() || !state_op.location.eq(GCounter.LOC_SUM_UUID)) {
             this._value = NaN;
         } else {
             this._value = state_op.value(0);
         }
     }
 
-    static reduce (old_state_i, change_frame_i, new_state_frame) {
-        const old = old_state_i.next();
-        let sum = old.done ? 0 : old.value.value(0);
+    static _reduce (old_state_i, change_frame_i, new_state_frame) {
+        const old = old_state_i.op;
+        let sum = old.isError() ? 0 : old.value(0); // TODO error cond tests
         if (!Number.isInteger(sum))
             return "malformed state";
-        for(let op of change_frame_i) {
-            const add = op.value(0);
+        let last_stamp = old.isError() ? UUID.ZERO : old.event; // FIXME
+        while (!change_frame_i.end()) {
+            const add = change_frame_i.op.value(0);
             if (!Number.isInteger(add))
                 return "malformed increment";
             sum += add;
             if (sum>Number.MAX_SAFE_INTEGER)
                 return "overflow";
+            last_stamp = change_frame_i.op.event;
+            change_frame_i.nextOp();
         }
-        new_state_frame.push(null, null, null, GCounter.SUM_UUID, sum);
+        new_state_frame.push(new Op(
+            new_state_frame._last_op.type,
+            new_state_frame._last_op.object,
+            last_stamp,
+            GCounter.LOC_SUM_UUID,
+            Op.js2ron(sum)
+        ));
+    }
+
+    static create (value) {
+        const i = Number.isInteger(value) ? value : 0;
+        const template = new Frame();
+        template.push( GCounter.TYPE_UUID, "1", "1", GCounter.LOC_SUM_UUID, i );
+        return template;
     }
 
 }
 
-GCounter.SUM_UUID = new UUID();
+GCounter.TYPE_UUID = UUID.fromString("inc");
+GCounter.LOC_SUM_UUID = UUID.fromString("sum");
+GCounter.LOC_ADD_UUID = UUID.fromString("add");
+RDT.TYPES[GCounter.TYPE_UUID] = GCounter;
+GCounter.REDUCER_FEATURES = RDT.FLAGS.OP_BASED|RDT.FLAGS.PATCH_BASED;
 
 module.exports = GCounter;
--- swarm-rdt/test/02_GCounter.js
+++ swarm-rdt/test/02_GCounter.js
@@ -1,3 +1,42 @@
-/**
- * Created by nodedev on 6/5/17.
- */
+"use strict";
+const tap = require('tape').test;
+const RDT = require('../src/RDT');
+const GCounter = require('../src/GCounter');
+
+
+tap ('rdt.02.A counter', function (t) {
+
+    const i = new GCounter(null);
+
+    t.equal(i.version(), '0', 'version comes from an op');
+    t.equal(i.id(), '0', 'id comes from an op');
+
+    const init = ".inc#time-author@`!";
+
+    i.update(init, '');
+    t.ok(i.hasState());
+    t.ok(i.hasIdentity());
+    t.equal(i.id(), 'time-author', 'id comes from an op');
+    t.equal(i.value(), 0);
+
+    const update = ".inc#time-author@`[1:inc=1";
+    const updated = RDT.reduce(init, update);
+
+    t.equal(updated.toString(), ".inc#time-author`[1!:sum=1");
+
+    i.update(updated, update);
+
+    t.equal(i.version(), 'time01-author', 'version id OK');
+    t.equal(i.id(), 'time-author', 'id OK');
+    t.equal(i.Id().origin, 'author');
+    t.ok(i.hasState());
+    t.ok(i.hasIdentity());
+    t.equals(i.value(), 1);
+    t.equal(i.version(), "time01-author");
+
+    // TODO close
+
+    // TODO create, inc, API
+
+    t.end();
+});
