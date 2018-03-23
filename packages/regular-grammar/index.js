62b711355047d0fb76ba59187573f8ecddb01b42
olebedev <ole6edev@gmail.com>
2018-03-23 10:40:42 +0500

    grammar: migrate to  pure ES5

--- packages/regular-grammar/index.js
+++ packages/regular-grammar/index.js
@@ -1,14 +1,14 @@
-module.exports = function resolve (rule_name, rules) {
-    const rule = rules[rule_name];
-    const pattern = rule.source.replace(/\$(\w+)/g, (match, name) => {
-        const parser = resolve(name, rules);
-        const pattern = parser.source.
-            replace(/\((?!\?:)/g, '(?:').
-            replace(/(\\\\)*\\\(\?:/g, '$1\\(');
-        return pattern;
-    });
+module.exports = function resolve(rule_name, rules) {
+  var rule = rules[rule_name];
+  var pattern = rule.source.replace(/\$(\w+)/g, function(match, name) {
+    var parser = resolve(name, rules);
+    var pattern = parser.source
+      .replace(/\((?!\?:)/g, '(?:')
+      .replace(/(\\\\)*\\\(\?:/g, '$1\\(');
+    return pattern;
+  });
 
-    return pattern===rule.pattern ? rule :
-        rules[rule_name] = new RegExp(pattern);
-
-}
+  return pattern === rule.pattern
+    ? rule
+    : (rules[rule_name] = new RegExp(pattern));
+};
