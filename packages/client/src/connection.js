ac55e51d0b577a284f3ebabf524be4422fe78b51
olebedev <ole6edev@gmail.com>
2018-04-03 15:34:49 +0500

    client: add debug connection wraper

--- packages/client/src/connection.js
+++ packages/client/src/connection.js
@@ -1,5 +1,7 @@
 // @flow
 
+import RWS from './rws';
+
 export interface Connection {
   onmessage: (ev: MessageEvent) => any;
   onopen: (ev: Event) => any;
@@ -24,4 +26,60 @@ export class DevNull implements Connection {
   send(data: string): void {}
   close(): void {}
   open(): void {}
+}
+
+export class Verbose extends RWS implements Connection {
+  _om: (ev: MessageEvent) => any;
+  _oo: (ev: Event) => any;
+
+  constructor(url: string, protocols: string[] = [], options: {} = {}) {
+    super(url, protocols, options);
+    const send = this.send;
+    this.send = (data: string): void => {
+      console.log(
+        '%c(≶) %c%s %c%s',
+        'color: blue',
+        'color: green;',
+        '~>',
+        'color: #aaa',
+        data,
+      );
+      send(data);
+    };
+  }
+
+  get onopen(): (ev: Event) => any {
+    return (ev: Event) => {
+      console.log(
+        '%c(≶) %c%s',
+        'color: blue;',
+        'color: green;',
+        // $FlowFixMe
+        'connected to ' + this._url,
+      );
+      this._oo(ev);
+    };
+  }
+
+  set onopen(m: (ev: Event) => void): void {
+    this._oo = m;
+  }
+
+  get onmessage(): (ev: MessageEvent) => any {
+    return (ev: MessageEvent) => {
+      console.log(
+        '%c(≶) %c%s %c%s',
+        'color: blue;',
+        'color: red;',
+        '<~',
+        'color: #aaa',
+        ev.data,
+      );
+      this._om(ev);
+    };
+  }
+
+  set onmessage(m: (ev: MessageEvent) => any): void {
+    this._om = m;
+  }
 }
